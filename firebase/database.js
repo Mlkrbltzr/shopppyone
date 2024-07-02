b653779a20f4f8a9d99d664ae21b963e05d2d631
german <choppers.astudillo@gmail.com>
2024-07-01 22:49:10 -0400

    trabajando

---
+++ firebase/database.js
@@ -0,0 +1,3 @@
+export const apiKey = 'AIzaSyAG2ydz4di6HBB0z2FBlSKWb9HTm-VM54M'
+export const authUrl = 'https://identitytoolkit.googleapis.com/'
+export const baseUrl = 'https://shoppyone-80a44-default-rtdb.firebaseio.com'
